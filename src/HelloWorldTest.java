acc51fedac054d2dff69a40fec6fbfcf8322c9e6
Ancient-Foe <amirhowell@hotmail.com>
2017-03-20 13:13:19 +1030

    updated HelloWorldTest
    Added an output to test if pulling to eclipse works

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -5,6 +5,8 @@ public class HelloWorldTest {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Hello world " + (i + 1) + "!");
 		}
+		
+		System.out.println("This was added on github to test pulling to eclipse");
 
 	}
 
